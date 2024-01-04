e2779df19ffd8da6041ea27abae8805f455d76d2
toefuVox <ph.c11.java.eo12@gmail.com>
2024-01-04 19:44:51 +0900

    replace QueryGithub

--- start.go
+++ start.go
@@ -2,10 +2,11 @@ package main
 
 import (
 	"context"
+	"fmt"
 	"log"
-	"time"
 
 	"entdemo/ent"
+	"entdemo/ent/group"
 
 	_ "github.com/lib/pq"
 )
@@ -21,78 +22,21 @@ func main() {
         log.Fatalf("failed creating schema resources: %v", err)
     }
 
-    if err := CreateGraph(context.Background(), client); err != nil {
-        log.Fatalf("failed creating graph: %v", err)
-    }
+    QueryGithub(context.Background(), client)
+
 }
 
-
-func CreateGraph(ctx context.Context, client *ent.Client) error {
-    // First, create the users.
-    a8m, err := client.User.
-        Create().
-        SetAge(30).
-        SetName("Ariel").
-        Save(ctx)
+func QueryGithub(ctx context.Context, client *ent.Client) error {
+    cars, err := client.Group.
+        Query().
+        Where(group.Name("GitHub")). // (Group(Name=GitHub),)
+        QueryUsers().                // (User(Name=Ariel, Age=30),)
+        QueryCars().                 // (Car(Model=Tesla, RegisteredAt=<Time>), Car(Model=Mazda, RegisteredAt=<Time>),)
+        All(ctx)
     if err != nil {
-        return err
+        return fmt.Errorf("failed getting cars: %w", err)
     }
-    neta, err := client.User.
-        Create().
-        SetAge(28).
-        SetName("Neta").
-        Save(ctx)
-    if err != nil {
-        return err
-    }
-    // Then, create the cars, and attach them to the users created above.
-    err = client.Car.
-        Create().
-        SetModel("Tesla").
-        SetRegisteredAt(time.Now()).
-        // Attach this car to Ariel.
-        SetOwner(a8m).
-        Exec(ctx)
-    if err != nil {
-        return err
-    }
-    err = client.Car.
-        Create().
-        SetModel("Mazda").
-        SetRegisteredAt(time.Now()).
-        // Attach this car to Ariel.
-        SetOwner(a8m).
-        Exec(ctx)
-    if err != nil {
-        return err
-    }
-    err = client.Car.
-        Create().
-        SetModel("Ford").
-        SetRegisteredAt(time.Now()).
-        // Attach this car to Neta.
-        SetOwner(neta).
-        Exec(ctx)
-    if err != nil {
-        return err
-    }
-    // Create the groups, and add their users in the creation.
-    err = client.Group.
-        Create().
-        SetName("GitLab").
-        AddUsers(neta, a8m).
-        Exec(ctx)
-    if err != nil {
-        return err
-    }
-    err = client.Group.
-        Create().
-        SetName("GitHub").
-        AddUsers(a8m).
-        Exec(ctx)
-    if err != nil {
-        return err
-    }
-    log.Println("The graph was created successfully")
+    log.Println("cars returned:", cars)
+    // Output: (Car(Model=Tesla, RegisteredAt=<Time>), Car(Model=Mazda, RegisteredAt=<Time>),)
     return nil
 }
