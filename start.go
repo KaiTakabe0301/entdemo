6eaf2a9e194c583a9eea3d2932345f4b563e401a
toefuVox <ph.c11.java.eo12@gmail.com>
2024-01-04 20:14:26 +0900

    replace QueryGroupWithUsers

--- start.go
+++ start.go
@@ -6,8 +6,7 @@ import (
 	"log"
 
 	"entdemo/ent"
-	"entdemo/ent/car"
-	"entdemo/ent/user"
+	"entdemo/ent/group"
 
 	_ "github.com/lib/pq"
 )
@@ -23,33 +22,19 @@ func main() {
         log.Fatalf("failed creating schema resources: %v", err)
     }
 
-    QueryArielCars(context.Background(), client)
+    QueryGroupWithUsers(context.Background(), client)
 
 }
 
-func QueryArielCars(ctx context.Context, client *ent.Client) error {
-    // Get "Ariel" from previous steps.
-    a8m := client.User.
+func QueryGroupWithUsers(ctx context.Context, client *ent.Client) error {
+    groups, err := client.Group.
         Query().
-        Where(
-            user.HasCars(),
-            user.Name("Ariel"),
-        ).
-        OnlyX(ctx)
-    cars, err := a8m.                       // Get the groups, that a8m is connected to:
-            QueryGroups().                  // (Group(Name=GitHub), Group(Name=GitLab),)
-            QueryUsers().                   // (User(Name=Ariel, Age=30), User(Name=Neta, Age=28),)
-            QueryCars().                    // (Car(Model=Tesla, RegisteredAt=<Time>), Car(Model=Mazda, RegisteredAt=<Time>),)
-            Where(                          //
-                car.Not(                    //  Get Neta and Ariel cars, but filter out
-                    car.Model("Mazda"),     //  those who named "Mazda"
-                ),                          //
-            ).                              //
-            All(ctx)
+        Where(group.HasUsers()).
+        All(ctx)
     if err != nil {
-        return fmt.Errorf("failed getting cars: %w", err)
+        return fmt.Errorf("failed getting groups: %w", err)
     }
-    log.Println("cars returned:", cars)
-    // Output: (Car(Model=Tesla, RegisteredAt=<Time>), Car(Model=Ford, RegisteredAt=<Time>),)
+    log.Println("groups returned:", groups)
+    // Output: (Group(Name=GitHub), Group(Name=GitLab),)
     return nil
 }
