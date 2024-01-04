2f0d62385854606501116cdfccc03a2968e8a112
toefuVox <ph.c11.java.eo12@gmail.com>
2024-01-04 19:23:06 +0900

    replace CreateGraph

--- start.go
+++ start.go
@@ -2,11 +2,10 @@ package main
 
 import (
 	"context"
-	"fmt"
 	"log"
+	"time"
 
 	"entdemo/ent"
-	"entdemo/ent/user"
 
 	_ "github.com/lib/pq"
 )
@@ -22,28 +21,76 @@ func main() {
         log.Fatalf("failed creating schema resources: %v", err)
     }
 
-    a8m, err := client.User.Query().Where(user.NameEQ("a8m")).All(context.Background())
-    if err != nil {
-        log.Fatalf("failed querying user: %v", err)
-    }
-
-
-    QueryCarUsers(context.Background(), a8m[1])
+    CreateGraph(context.Background(), client)
 }
 
 
-func QueryCarUsers(ctx context.Context, a8m *ent.User) error {
-    cars, err := a8m.QueryCars().All(ctx)
+func CreateGraph(ctx context.Context, client *ent.Client) error {
+    // First, create the users.
+    a8m, err := client.User.
+        Create().
+        SetAge(30).
+        SetName("Ariel").
+        Save(ctx)
     if err != nil {
-        return fmt.Errorf("failed querying user cars: %w", err)
+        return err
     }
-    // Query the inverse edge.
-    for _, c := range cars {
-        owner, err := c.QueryOwner().Only(ctx)
-        if err != nil {
-            return fmt.Errorf("failed querying car %q owner: %w", c.Model, err)
-        }
-        log.Printf("car %q owner: %q\n", c.Model, owner.Name)
+    neta, err := client.User.
+        Create().
+        SetAge(28).
+        SetName("Neta").
+        Save(ctx)
+    if err != nil {
+        return err
     }
+    // Then, create the cars, and attach them to the users created above.
+    err = client.Car.
+        Create().
+        SetModel("Tesla").
+        SetRegisteredAt(time.Now()).
+        // Attach this car to Ariel.
+        SetOwner(a8m).
+        Exec(ctx)
+    if err != nil {
+        return err
+    }
+    err = client.Car.
+        Create().
+        SetModel("Mazda").
+        SetRegisteredAt(time.Now()).
+        // Attach this car to Ariel.
+        SetOwner(a8m).
+        Exec(ctx)
+    if err != nil {
+        return err
+    }
+    err = client.Car.
+        Create().
+        SetModel("Ford").
+        SetRegisteredAt(time.Now()).
+        // Attach this car to Neta.
+        SetOwner(neta).
+        Exec(ctx)
+    if err != nil {
+        return err
+    }
+    // Create the groups, and add their users in the creation.
+    err = client.Group.
+        Create().
+        SetName("GitLab").
+        AddUsers(neta, a8m).
+        Exec(ctx)
+    if err != nil {
+        return err
+    }
+    err = client.Group.
+        Create().
+        SetName("GitHub").
+        AddUsers(a8m).
+        Exec(ctx)
+    if err != nil {
+        return err
+    }
+    log.Println("The graph was created successfully")
     return nil
 }
